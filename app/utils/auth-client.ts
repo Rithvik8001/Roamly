357eaddbbe02b4641014958b40c970e2ef97bbee
Rithvik <1017rithvik@gmail.com>
2025-09-10 17:15:52 -0400

    bug fixes

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -1,7 +1,18 @@
 import { createAuthClient } from "better-auth/client";
 
+function resolveClientBaseURL(): string {
+  if (typeof window !== "undefined" && window.location?.origin) {
+    return window.location.origin;
+  }
+  return (
+    process.env.NEXT_PUBLIC_BASE_URL ||
+    process.env.BETTER_AUTH_URL ||
+    "http://localhost:3000"
+  );
+}
+
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: resolveClientBaseURL(),
 });
 
 export const { signIn, signUp, signOut, useSession, getSession } = authClient;
